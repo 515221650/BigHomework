380715f664d283f24ecdf90218506aab141b68ae
yangzhuo18 <yangzhuo18@mails.tsinghua.edu.cn>
2020-09-04 12:47:09 +0800

    add bottom coin

--- app/src/main/java/com/example/bighomework/MainActivity.java
+++ app/src/main/java/com/example/bighomework/MainActivity.java
@@ -3,14 +3,47 @@ package com.example.bighomework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TableLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.bighomework.common.DefineView;
+
+public class MainActivity extends AppCompatActivity implements DefineView {
+
+    private RadioButton rb1,rb2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initValidData();
+        initListener();
+        binData();
+    }
+
+    @Override
+    public void initView() {
+        rb1 = (RadioButton)findViewById(R.id.rb1);
+        rb2 = (RadioButton)findViewById(R.id.rb2);
+
+    }
+
+    @Override
+    public void initValidData() {
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void binData() {
 
     }
 }
