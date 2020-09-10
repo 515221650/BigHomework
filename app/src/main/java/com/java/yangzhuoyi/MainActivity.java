a71cad323ae6729d1b221cbdf344e480adb3c696
wenyihong <wenyihong2000@126.com>
2020-09-10 22:44:17 +0800

    revising ui

--- app/src/main/java/com/java/yangzhuoyi/MainActivity.java
+++ app/src/main/java/com/java/yangzhuoyi/MainActivity.java
@@ -71,22 +71,22 @@ public class MainActivity extends AppCompatActivity implements DefineView {
 
 
         mainInfoFragment = new MainInfoFragment();
-//        dataFragment = new DataFragment();
+        dataFragment = new DataFragment();
         expertFragment = new ExpertFragment();
         knowledgeGraphFragment = new KnowledgeGraphFragment();
-//        eventFragment = new EventFragment();
+        eventFragment = new EventFragment();
 
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
-//        transaction.add(R.id.fragment_container, eventFragment);
+        transaction.add(R.id.fragment_container, eventFragment);
         transaction.add(R.id.fragment_container, knowledgeGraphFragment);
-//        transaction.add(R.id.fragment_container, dataFragment);
+        transaction.add(R.id.fragment_container, dataFragment);
         transaction.add(R.id.fragment_container, expertFragment);
         transaction.add(R.id.fragment_container, mainInfoFragment);
 
-//        transaction.hide(eventFragment);
+        transaction.hide(eventFragment);
         transaction.hide(expertFragment);
-//        transaction.hide(dataFragment);
+        transaction.hide(dataFragment);
         transaction.hide(knowledgeGraphFragment);
 
 
